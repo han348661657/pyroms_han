532ec7ba46b6d259d1212eed4b1f62ec93f69e7c
han348661657 <hzh_1211@163.com>
2018-08-19 20:36:19 +0800

    study pyroms

---
+++ hanr.py
@@ -0,0 +1,8 @@
+#!/data/home/henry/anaconda3/bin/python3.6
+# -*- coding: utf-8 -*-
+# @File  : hanr.py
+# @Author: Hanzhaohui
+# @Date  : 2018/8/17
+# @Desc  : surface waves effects
+
+print('sdfg')
---
+++ test1.py
@@ -0,0 +1,13 @@
+#!/data/home/henry/anaconda3/bin/python3.6
+# -*- coding: utf-8 -*-
+# @File  : test1.py
+# @Author: Hanzhaohui
+# @Date  : 2018/8/17
+# @Desc  : surface waves effects
+
+print('good')
+print('hello')
+
+print('good')
+print('hello')
+
